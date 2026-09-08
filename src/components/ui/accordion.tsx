57c9f367661d84df5b8ac23fe4777b010a2bc6df
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-08 21:08:28 +0700

    feat: update Accordion component with new icons and improved styling in FaqSection

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { cn } from "cn"
-import { Accordion as AccordionPrimitive } from "radix-ui"
-import { ChevronDownIcon, ChevronUpIcon } from "lucide-react"
+import * as React from 'react';
+import { cn } from 'cn';
+import { Accordion as AccordionPrimitive } from 'radix-ui';
+import { LuMinus, LuPlus } from 'react-icons/lu';
 
 function Accordion({
   className,
@@ -9,11 +9,11 @@ function Accordion({
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
   return (
     <AccordionPrimitive.Root
-      data-slot="accordion"
-      className={cn("flex w-full flex-col", className)}
+      data-slot='accordion'
+      className={cn('flex w-full flex-col gap-4 lg:gap-6', className)}
       {...props}
     />
-  )
+  );
 }
 
 function AccordionItem({
@@ -22,11 +22,11 @@ function AccordionItem({
 }: React.ComponentProps<typeof AccordionPrimitive.Item>) {
   return (
     <AccordionPrimitive.Item
-      data-slot="accordion-item"
-      className={cn("not-last:border-b", className)}
+      data-slot='accordion-item'
+      className={cn('', className)}
       {...props}
     />
-  )
+  );
 }
 
 function AccordionTrigger({
@@ -35,21 +35,26 @@ function AccordionTrigger({
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
   return (
-    <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Header className='flex'>
       <AccordionPrimitive.Trigger
-        data-slot="accordion-trigger"
+        data-slot='accordion-trigger'
         className={cn(
-          "group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg border border-transparent py-2.5 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4 **:data-[slot=accordion-trigger-icon]:text-muted-foreground",
+          'group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg border border-transparent py-2.5 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:size-4',
           className
         )}
         {...props}
       >
         {children}
-        <ChevronDownIcon data-slot="accordion-trigger-icon" className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden" />
-        <ChevronUpIcon data-slot="accordion-trigger-icon" className="pointer-events-none hidden shrink-0 group-aria-expanded/accordion-trigger:inline" />
+        <div className='border rounded-full size-7 lg:size-8 shrink-0 flex justify-center items-center pointer-events-none  group-aria-expanded/accordion-trigger:hidden'>
+          <LuPlus data-slot='accordion-trigger-icon' className='stroke-3' />
+        </div>
+
+        <div className='border rounded-full size-7 lg:size-8  justify-center items-center group-aria-expanded/accordion-trigger:flex pointer-events-none hidden shrink-0'>
+          <LuMinus data-slot='accordion-trigger-icon' className='stroke-3' />
+        </div>
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  )
+  );
 }
 
 function AccordionContent({
@@ -59,20 +64,22 @@ function AccordionContent({
 }: React.ComponentProps<typeof AccordionPrimitive.Content>) {
   return (
     <AccordionPrimitive.Content
-      data-slot="accordion-content"
-      className="overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up"
+      data-slot='accordion-content'
+      className='overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up'
       {...props}
     >
       <div
         className={cn(
-          "h-(--radix-accordion-content-height) pt-0 pb-2.5 [&_a]:underline [&_a]:underline-offset-3 [&_a]:hover:text-foreground [&_p:not(:last-child)]:mb-4",
+          ' pt-0 pb-2.5 [&_a]:underline [&_a]:underline-offset-3 [&_a]:hover:text-foreground [&_p:not(:last-child)]:mb-4',
           className
         )}
       >
         {children}
       </div>
     </AccordionPrimitive.Content>
-  )
+  );
 }
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+
+// h-(--radix-accordion-content-height)
--- src/sections/08-faq/FaqSection.tsx
+++ src/sections/08-faq/FaqSection.tsx
@@ -1,12 +1,38 @@
 import SectionWrapper from '@/components/layouts/SectionWrapper';
 import SectionTitle from '@/components/shared/SectionTitle';
 import { faqData } from '@/data/10-faqData';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const FaqSection = () => {
   const header = faqData.header;
+  const faqs = faqData.questions;
   return (
     <SectionWrapper sectionId='faq'>
       <SectionTitle title={header.title} badgeText={header.badge} />
+      <Accordion type='single' collapsible defaultValue='approach'>
+        {faqs.map((item) => (
+          <AccordionItem
+            value={item.value}
+            key={item.value}
+            className=' rounded-xl p-px bg-neutral-300 data-[state=open]:bg-gradient-purple '
+          >
+            <div className='relative rounded-xl px-4 lg:px-8 py-5 bg-neutral-50 overflow-hidden '>
+              <div className='absolute inset-y-0 w-[6px] lg:w-2.5 left-0 bg-primary-700' />
+              <AccordionTrigger className='text-md font-semibold lg:text-xl data-[state=open]:text-primary-300 cursor-pointer'>
+                {item.trigger}
+              </AccordionTrigger>
+              <AccordionContent className='text-sm lg:text-md'>
+                {item.content}
+              </AccordionContent>
+            </div>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </SectionWrapper>
   );
 };
